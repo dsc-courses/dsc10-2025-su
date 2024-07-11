4dd9f91a6d7371a61a0788dfae00982fabec5218
Jason Huynh <79772606+jasxnhuynh@users.noreply.github.com>
2024-07-11 09:24:16 -0700

    releasing lab04 and hw03

---
+++ homeworks/hw03/tests/q5_3.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q5_3',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(free_housing_chance, float) and 0 < free_housing_chance < 1 \nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> int(free_housing_chance * 10 ** 16 % 10 ** 2) == 40 # Try again!\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
