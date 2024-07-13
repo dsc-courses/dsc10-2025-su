716e02e00c22ca15cd93a51f37c000ec6b0e5184
Jason Huynh <79772606+jasxnhuynh@users.noreply.github.com>
2024-07-13 11:18:17 -0700

    publish lab5, hw4, and hw5.

---
+++ homeworks/hw04/tests/q3_3.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3_3',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(jasmine_lower_bound, float) and isinstance(jasmine_upper_bound, float)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 0.0 <= jasmine_lower_bound < jasmine_proportion <= jasmine_upper_bound <= 1.0\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ homeworks/hw05/tests/q2_4.py
@@ -0,0 +1,13 @@
+test = {   'name': 'q2_4',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(theoretical_counts_mean, float) and isinstance(actual_counts_mean, float)\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> theoretical_counts_mean != 114 # Your sample is smaller than the population, so you expect fewer water types in the sample than in the '
+                                               'population.\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> int(theoretical_counts_mean * 10**13 % 10**4) == 6554  # Try again!\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
