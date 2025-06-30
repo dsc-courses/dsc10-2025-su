29bedaf19afc84044bfac7bed732dfcfb591fece
a1ho <a1ho@ucsd.edu>
2025-06-29 21:19:12 -0700

    clear labs folder

--- labs/lab04/tests/q3_5.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q3_5',
-    'points': None,
-    'suites': [{'cases': [{'code': '>>> np.isclose(q3_5, 0.6432138)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
