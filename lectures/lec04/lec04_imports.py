eae213d887250c0e6acef23031cfc9732aa20cc3
chens5 <srchen98@gmail.com>
2025-07-07 00:00:12 -0700

    lecture 04

---
+++ lectures/lec04/lec04_imports.py
@@ -0,0 +1,26 @@
+import numpy as np
+import babypandas as bpd
+import pandas as pd
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = (10, 5)
+
+np.set_printoptions(threshold=20, precision=2, suppress=True)
+pd.set_option("display.max_rows", 7)
+pd.set_option("display.max_columns", 8)
+pd.set_option("display.precision", 2)
+
+from IPython.display import display, IFrame, YouTubeVideo
+
+def show_grouping_animation():
+    src = "https://docs.google.com/presentation/d/e/2PACX-1vTgVlFngQcLMYHP-z1vq5lVXjsBgcHebc-3TX7SW6L_gjX6TD1gsflvVDQUpWiDdeEPqJASenUIfBVd/embed?start=false&loop=false&delayms=60000&rm=minimal"
+    width = 960
+    height = 509
+    display(IFrame(src, width, height))
+
+def binning_animation():
+    src="https://docs.google.com/presentation/d/e/2PACX-1vTnRGwEnKP2V-Z82DlxW1b1nMb2F0zWyrXIzFSpQx_8Wd3MFaf56y2_u3JrLwZ5SjWmfapL5BJLfsDG/embed?start=false&loop=false&delayms=60000&rm=minimal"
+    width=900
+    height=270
+    display(IFrame(src, width, height))
